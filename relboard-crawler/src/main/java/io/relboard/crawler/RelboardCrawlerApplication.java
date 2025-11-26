4538a419ff99158fba8f19d0d4db36662661da0e
Bellmin <whdals011203@naver.com>
2025-11-26 23:11:38 +0900

    chore: align project setup with Spring Boot 4 and io.relboard package

---
+++ relboard-crawler/src/main/java/io/relboard/crawler/RelboardCrawlerApplication.java
@@ -0,0 +1,16 @@
+package io.relboard.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@EnableJpaAuditing
+@SpringBootApplication
+public class RelboardCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelboardCrawlerApplication.class, args);
+    }
+}
