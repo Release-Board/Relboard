4d67ebbd2c99ddde44bbf0e2f9b1fceb9064fdb3
Bellmin <whdals011203@naver.com>
2025-11-26 13:48:18 +0900

    chore: 프로젝트 초기 세팅

---
+++ relboard-crawler/src/main/java/io/relboard/crawler/RelboardApplication.java
@@ -0,0 +1,13 @@
+package io.relboard.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelboardApplication.class, args);
+    }
+
+}
---
+++ relboard-crawler/src/test/java/io/relboard/crawler/RelboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.relboard.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
