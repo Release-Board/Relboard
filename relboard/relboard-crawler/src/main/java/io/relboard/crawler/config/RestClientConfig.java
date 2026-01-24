e9e7607a06626d690b2465bcf0be51349286caf2
Bellmin <whdals011203@naver.com>
2026-01-24 16:34:19 +0900

    feat: RestClientConfig 추가

--- relboard/relboard-crawler/src/main/java/io/relboard/crawler/config/RestClientConfig.java
+++ relboard/relboard-crawler/src/main/java/io/relboard/crawler/config/RestClientConfig.java
@@ -1,8 +1,11 @@
 package io.relboard.crawler.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,6 +13,23 @@ public class RestClientConfig {
 
   @Bean
   public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+
+  @Bean
+  @Qualifier("githubRestClient")
+  public RestClient githubRestClient(
+      RestClient.Builder builder, @Value("${github.token:}") String githubToken) {
+    RestClient.Builder configured = builder.baseUrl("https://api.github.com");
+    if (githubToken != null && !githubToken.isBlank()) {
+      configured = configured.defaultHeader("Authorization", "Bearer " + githubToken);
+    }
+    return configured.defaultHeader("Accept", "application/vnd.github+json").build();
+  }
+
+  @Bean
+  @Qualifier("mavenRestClient")
+  public RestClient mavenRestClient(RestClient.Builder builder) {
     return builder.build();
   }
 }
